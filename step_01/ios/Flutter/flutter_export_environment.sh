ef3ce0ca52905c8e95e886d2a49771196290d849
Ashish-Raturi <ashishraturi368@gmail.com>
2022-04-03 23:43:11 +0530

    code_updated

---
+++ step_01/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ashishraturi/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ashishraturi/ar work/Flutter work/Projects/Courses/2. Flutter In App Purchase/Code/step_01"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
